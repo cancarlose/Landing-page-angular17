a9c01403348b60e8d7dfecdc34e45cd615650a3f
carlos <carlosedlima4@gmail.com>
2024-03-12 12:57:31 -0300

    docs development of the benefits component and creation of the main button component

---
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-primary',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-primary.component.html',
+  styleUrl: './btn-primary.component.scss'
+})
+export class BtnPrimaryComponent {
+
+}
