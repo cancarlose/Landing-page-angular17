14e080f8814064f12863d3ec028a3aec375c971f
carlos <carlosedlima4@gmail.com>
2024-03-13 12:21:06 -0300

    docs update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,19 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { BtnPrimaryComponent } from "../btn-primary/btn-primary.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    HeaderComponent,
-    NgOptimizedImage
-  ],
-  providers: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    providers: [],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [
+        HeaderComponent,
+        NgOptimizedImage,
+        BtnPrimaryComponent
+    ]
 })
 export class HomeComponent {
 
