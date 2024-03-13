4a7ff9b1aa299a145b83e205da1e4404b2f18e2d
carlos <carlosedlima4@gmail.com>
2024-03-13 12:35:58 -0300

    docs create newsletter-form component

---
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newsletter-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './newsletter-form.component.html',
+  styleUrl: './newsletter-form.component.scss'
+})
+export class NewsletterFormComponent {
+
+}
