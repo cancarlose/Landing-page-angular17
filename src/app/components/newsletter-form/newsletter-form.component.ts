6b54f48fc5acbdf29363e68b142af83fea556b57
carlos <carlosedlima4@gmail.com>
2024-03-18 12:52:07 -0300

    docs update

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,6 +1,7 @@
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BtnPrimaryComponent } from './../btn-primary/btn-primary.component';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NewsletterService } from '../../services/newsletter.service';
 
 @Component({
   selector: 'app-newsletter-form',
@@ -9,16 +10,32 @@ import { Component } from '@angular/core';
     BtnPrimaryComponent,
     ReactiveFormsModule
   ],
+  providers: [
+    NewsletterService,
+  ],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
 export class NewsletterFormComponent {
   newsletterForm!: FormGroup;
+  loading = signal(false);
 
-  constructor() {
+  constructor(private service: NewsletterService) {
     this.newsletterForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email])
-    })
+    });
   }
+
+    onSubmit(){
+      this.loading.set(true);
+
+      if(this.newsletterForm.valid){
+        this.service.sendData(this.newsletterForm.value.name, this.newsletterForm.value.email).subscribe({
+          next: () => {
+            this.newsletterForm.reset();
+          }
+        });
+      }
+    }
 }
